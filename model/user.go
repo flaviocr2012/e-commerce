fddc1bb323210bbb7ce3a110b6c3150537e8dea9
flaviocr2012 <flaviocr2012@gmail.com>
2026-08-14 14:16:35 -0300

    feat: update models with address as string field

--- model/user.go
+++ model/user.go
@@ -10,7 +10,7 @@ type User struct {
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
 	Role      string    `json:"role"`
-	Address   Address   `json:"address"`
+	Address   string   `json:"address"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
